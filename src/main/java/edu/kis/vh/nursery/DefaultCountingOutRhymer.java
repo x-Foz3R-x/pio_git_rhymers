493105408bfbe9a9c0bab2d9f7082b949a096065
Foz3R <pawel.knot.1@gmail.com>
2026-04-08 13:57:06 +0200

    Opisanie pliku komentarzami

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,7 +1,10 @@
 package edu.kis.vh.nursery;
 
 
-
+/**
+ * Klasa implementująca domyślną wyliczankę opartą na tablicy.
+ * Działa na zasadzie prostej struktury stosu, przechowując liczby całkowite.
+ */
 public class DefaultCountingOutRhymer {
 
     private static final int SIZE = 12;
@@ -11,29 +14,53 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY;
 
+    /**
+     * Zwraca aktualny wskaźnik na ostatnio dodany element.
+     * @return wartość wskaźnika total
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * Dodaje nową liczbę do wyliczanki, jeśli nie jest ona pełna.
+     * @param in wartość całkowita do dodania
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Sprawdza, czy wyliczanka jest pusta.
+     * @return true, jeśli pusta, w przeciwnym razie false
+     */
     public boolean callCheck() {
         return total == EMPTY;
     }
 
+    /**
+     * Sprawdza, czy wyliczanka jest pełna.
+     * @return true, jeśli pełna, w przeciwnym razie false
+     */
     public boolean isFull() {
         return total == SIZE - 1;
     }
 
+    /**
+     * Zwraca wartość ostatnio dodanego elementu bez jego usuwania.
+     * @return ostatnio dodana liczba lub wartość domyślna w przypadku pustej wyliczanki
+     */
     protected int peekaboo() {
         if (callCheck())
             return -1;
         return numbers[total];
     }
 
+    /**
+     * Zwraca wartość ostatnio dodanego elementu i usuwa go z wyliczanki.
+     * @return pobrana liczba lub wartość domyślna w przypadku pustej wyliczanki
+     */
     public int countOut() {
         if (callCheck())
             return -1;
